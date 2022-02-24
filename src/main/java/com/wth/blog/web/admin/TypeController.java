c148c458591b0cea527a099469fc6946a2ab620d
wangtianhao <wangthwork@163.com>
2022-02-24 10:58:55 +0800

    20220224

--- src/main/java/com/wth/blog/web/admin/TypeController.java
+++ src/main/java/com/wth/blog/web/admin/TypeController.java
@@ -11,6 +11,7 @@ import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.servlet.mvc.support.RedirectAttributes;
 
 @Controller
 @RequestMapping("/admin")
@@ -30,12 +31,12 @@ public class TypeController {
     }
 
     @PostMapping("/types")
-    public String post(Type type) {
+    public String post(Type type, RedirectAttributes attributes) {
         Type t = typeService.saveType(type);
         if (t == null) {
-            //
+            attributes.addFlashAttribute("message","操作失败");
         } else {
-            //
+            attributes.addFlashAttribute("message","操作成功");
         }
         return "redirect:/admin/types";
     }
