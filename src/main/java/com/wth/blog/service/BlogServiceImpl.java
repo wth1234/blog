81cf6c4b3b05af571389a0e3d76cff749785cf1d
wangtianhao <wangthwork@163.com>
2022-03-08 21:30:25 +0800

    20220308 初步完成博客首页的展示功能

--- src/main/java/com/wth/blog/service/BlogServiceImpl.java
+++ src/main/java/com/wth/blog/service/BlogServiceImpl.java
@@ -9,7 +9,9 @@ import com.wth.blog.vo.BlogQuery;
 import org.springframework.beans.BeanUtils;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.data.domain.Page;
+import org.springframework.data.domain.PageRequest;
 import org.springframework.data.domain.Pageable;
+import org.springframework.data.domain.Sort;
 import org.springframework.data.jpa.domain.Specification;
 import org.springframework.stereotype.Service;
 
@@ -56,6 +58,13 @@ public class BlogServiceImpl implements BlogService{
     @Override
     public Page<Blog> listBlog(Pageable pageable) {
         return blogRepository.findAll(pageable);
+    }
+
+    @Override
+    public List<Blog> listRecommendBlogTop(Integer size) {
+        Sort sort=Sort.by(Sort.Direction.DESC,"updateTime");
+        Pageable pageable= PageRequest.of(0,size,sort);
+        return blogRepository.findTop(pageable);
     }
 
     @Transactional
--- src/main/java/com/wth/blog/web/indexController.java
+++ src/main/java/com/wth/blog/web/indexController.java
@@ -28,9 +28,10 @@ public class indexController {
         model.addAttribute("page",blogService.listBlog(pageable));
         model.addAttribute("types",typeService.listTypeTop(6));
         model.addAttribute("tags",tagService.listTagTop(10));
+        model.addAttribute("recommendBlogs",blogService.listRecommendBlogTop(8));
         return "index";
     }
-    @GetMapping("/blog")
+    @GetMapping("/blog/{id}")
     public  String blog(){
         return "blog ";
     }
