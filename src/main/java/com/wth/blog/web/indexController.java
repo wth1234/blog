ca78d54f524f07352199543446d63443b6142374
wangtianhao <wangthwork@163.com>
2022-03-10 11:27:10 +0800

    20220310 完成首页搜索功能

--- src/main/java/com/wth/blog/web/indexController.java
+++ src/main/java/com/wth/blog/web/indexController.java
@@ -12,8 +12,7 @@ import org.springframework.data.domain.Sort;
 import org.springframework.data.web.PageableDefault;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.*;
 
 @Controller
 public class indexController {
@@ -31,8 +30,16 @@ public class indexController {
         model.addAttribute("recommendBlogs",blogService.listRecommendBlogTop(8));
         return "index";
     }
+    @PostMapping("/search")
+    public  String search(@PageableDefault(size=3,sort = {"updateTime"},direction = Sort.Direction.DESC) Pageable pageable, @RequestParam String query, Model model){
+        model.addAttribute("page",blogService.listBlog("%"+query+"%",pageable));
+        model.addAttribute("query",query);
+        return "search";
+    }
+
     @GetMapping("/blog/{id}")
     public  String blog(){
         return "blog ";
     }
+
 }
